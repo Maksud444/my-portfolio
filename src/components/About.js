1bb32dc3c1f47a363c8dc3bf74550992eedd9f00
Maksud444 <83550908+Maksud444@users.noreply.github.com>
2022-12-09 18:49:57 +0600

    aded netkify deploy

--- src/components/About.js
+++ src/components/About.js
@@ -1,44 +1,51 @@
-import React from 'react';
-import { Link } from 'react-scroll';
+import React from "react";
+import { Link } from "react-scroll";
 
 // import img
-import Image from '../assets/img/my-cover.jpg';
+import Image from "../assets/img/my-cover.jpg";
 
 const About = () => {
   return (
-    <section className='section bg-secondary' id='about'>
-      <div className='container mx-auto'>
-        <div className='flex flex-col xl:flex-row gap-24'>
+    <section className="section bg-secondary" id="about">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-24">
           <img
-            className='object-cover h-full w-[566px] md:mx-auto lg:mx-0 rounded-2xl'
+            className="object-cover h-full w-[566px] md:mx-auto lg:mx-0 rounded-2xl"
             src={Image}
-            alt=''
+            alt=""
           />
-          <div className='flex flex-col items-center text-center lg:items-start lg:text-left' data-aos="fade-up"
-          data-aos-anchor-placement="top-center" data-aos-duration="2000">
-            <div className='flex flex-col' >
-              <h2  className='text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block'>
+          <div
+            className="flex flex-col items-center text-center lg:items-start lg:text-left"
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-center"
+            data-aos-duration="2000"
+          >
+            <div className="flex flex-col">
+              <h2 className="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block">
                 Maksud Billah
               </h2>
-              <p className='mb-4 text-accent'>
-                 Front-end Web Developer & Designer 
+              <p className="mb-4 text-accent">
+                Front-end Web Developer & Designer
               </p>
-              <hr className='mb-8 opacity-5' />
-              <p className='mb-8'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit tempore
-                earum dolorum quo consequatur rem nihil hic ducimus rerum soluta
-                neque harum velit molestiae dignissimos distinctio dolorum nisi
-                labore culpa nihil. <br />
-                <br />
-                Qui doloremque quod sit accusantium unde totam inventore
-                cupiditate ratione esse maxime eum recusandae ducimus.
+              <hr className="mb-8 opacity-5" />
+              <p className="mb-8">
+                ☛ I am Maksud Billah I am a full-stack web developer Expertise 
+                /
+                Skill Sets --------------------- ☛ Html Css Expert ☛ Javascript
+                Expert ☛ Bootstrap coded responsive PSD to HTML/Bootstrap ☛
+                Custom Javascript designed ☛ react js ☛ tailwind CSS ☛ Es6 ☛
+                React Router Dom ☛ HTML, CSS. BOOTSTRAP, JAVASCRIPT, Debug
+                Fixing. ☛ Node Mailer/Mailchimp/ Mailgun/ Get Response Email
+                Platform Expert ☛ firebase Authentication ☛Context API, dev tool
+                ☛Google Analytics ☛Node js ☛Express js ☛MongoDB database
+                integration ☛JWT ☛Payment integration (Stripe, Paypal)
               </p>
             </div>
-           <Link to='/Contact'>
-           <button className='btn btn-md bg-gradient-to-bl bg-accent transition-all'>
-              Contact me
-            </button>
-           </Link>
+            <Link to="/Contact">
+              <button className="btn btn-md bg-gradient-to-bl bg-accent transition-all">
+                Contact me
+              </button>
+            </Link>
           </div>
         </div>
       </div>
