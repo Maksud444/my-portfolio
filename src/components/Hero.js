b8bbb2de3fa366aeb116645070651aa04e6f75cb
Maksud444 <83550908+Maksud444@users.noreply.github.com>
2022-12-10 13:44:37 +0600

    aded typewirte

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
 
 // import woman image
 import WomanImg from '../assets/img/20tkwsksksadfaskd;.png';
@@ -6,6 +7,7 @@ import resume from '../assets/img/Maksud Billah  (1).pdf';
 import './Hero.css'
 
 const Hero = () => {
+
 
   const onButtonClick = () => {
     // using Java Script method to get PDF file
@@ -21,6 +23,8 @@ const Hero = () => {
         })
     })
   }
+
+ 
   return (
     <section
       id='home'
@@ -28,15 +32,33 @@ const Hero = () => {
     >
       <div className='container mx-auto h-full mb-6 mt-7'>
         <div className='flex items-center h-full pt-8'>
-          <div className='flex-1 flex flex-col items-center lg:items-start' data-aos="fade-up"
-           data-aos-anchor-placement="center-bottom" data-aos-duration="2000">
-            <h1 data-aos="fade-right" data-aos-anchor-placement="top-center" className='text-3xl leading-[34px] md:text-5xl md:leading-tight lg:text-5xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>
-              Hi, I Am Maksud Billah 
-            </h1>
-            <h1 className='text-3xl text-accent leading-[44px] md:text-5xl md:leading-tight lg:text-6xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>
+          <div className='flex-1 flex flex-col items-center lg:items-start' >
+            
+            <h1  className='text-3xl leading-[34px] md:text-5xl md:leading-tight lg:text-5xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>
+            Hi, I Am   {' '}
+        <span className='text-accent text-bold text-5xl'>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={['Maksud', 'Billah']}
+            loop={5}
+            cursor
+            cursorStyle='_'
+            typeSpeed={60}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </span>
+      </h1>
+            <h1 className='text-3xl text-accent leading-[44px] md:text-5xl md:leading-tight lg:text-6xl lg:leading-[1.2] font-bold md:tracking-[-2px]'
+            data-aos="fade-up"
+            data-aos-anchor-placement="center-bottom" data-aos-duration="2000"
+            >
               I Am a Professional
             </h1>
-            <h1 className='text-3xl leading-[44px] md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>
+            <h1 className='text-3xl leading-[44px] md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px]'
+            data-aos="fade-up"
+            data-aos-anchor-placement="center-bottom" data-aos-duration="2000"
+            >
                 Full Stack Web Developer.
             </h1>
             <button onClick={onButtonClick} className='btn btn-md bg-gradient-to-bl bg-accent transition-all'>
