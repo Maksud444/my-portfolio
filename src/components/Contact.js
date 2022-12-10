8ca1fb9ae2ee97f331fe94f8b4e3199e66558899
Maksud444 <83550908+Maksud444@users.noreply.github.com>
2022-12-10 12:50:30 +0600

    aded email send msg from email inbox

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
+import { Form, Input, TextArea, Button } from 'semantic-ui-react';
+import Swal from 'sweetalert2';
+
 
 // import contact data
 import { contact } from '../data';
 
+const SERVICE_ID = "service_eoi11b5";
+const TEMPLATE_ID = "template_lh0tlru";
+const USER_ID = "01WfV9_fd0tiUD7Nb";
 
 const Contact = () => {
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+      .then((result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: 'success',
+          title: 'Message Sent Successfully'
+        })
+      }, (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: 'error',
+          title: 'Ooops, something went wrong',
+          text: error.text,
+        })
+      });
+    e.target.reset()
+  };
   return (
     <section className='section bg-primary' id='contact'>
       <div className='container mx-auto'>
-        <div className='flex flex-col items-center text-center' data-aos="fade-up"
+        <div className='flex flex-col items-center text-center mb-5' data-aos="fade-up"
           data-aos-anchor-placement="top-center" data-aos-duration="2000">
           <h2 className='section-title before:content-contact relative before:absolute before:opacity-40 before:-top-7 before:-left-40 before:hidden before:lg:block'>
             Contact me
@@ -39,15 +65,20 @@ const Contact = () => {
               );
             })}
           </div>
-          <form
+          <form onSubmit={handleOnSubmit}
             className='space-y-8 w-full max-w-[780px]'
           >
             <div className='flex gap-8'>
-              <input className='input' type='text' placeholder='Your name' />
-              <input className='input' type='email' placeholder='Your email' />
+              <input className='input' type='text' id='form-input-control-last-name'
+          control={Input} required placeholder='Your name' />
+              <input className='input' type='email'  id='form-input-control-email'
+          control={Input} required placeholder='Your email' />
             </div>
             <input className='input' type='text' placeholder='Subject' />
             <textarea
+            id='form-textarea-control-opinion'
+            control={TextArea}
+            required
               className='textarea'
               placeholder='Your message'
             ></textarea>
